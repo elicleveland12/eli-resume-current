dab0578da95ae70c13dd7ca7e1022109115c0c93
Elijah Cleveland <elicleveland12@gmail.com>
2019-05-06 15:18:19 -0400

    burger button

--- src/Components/HiddenMenu.js
+++ src/Components/HiddenMenu.js
@@ -7,6 +7,7 @@ export default class HiddenMenu extends React.Component {
   render() {
     return (
       <div className="hiddenContainer" onMouseEnter={this.props.toggleSideBar}>
+        <img src={require('../public/burger-button.001.jpeg')} alt="Hamburger" className="burger-button" onClick={this.props.toggleSideBar}/>
       </div>
     );
   }
